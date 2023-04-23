5c1bb7b093f012d45f54f22a028406b2587b9704
sanonesan <ezcudze@gmail.com>
2023-04-23 16:04:41 +0300

    save

--- sem5/SLE_methods/src/include/SLE_methods/banded_sle.hpp
+++ sem5/SLE_methods/src/include/SLE_methods/banded_sle.hpp
@@ -10,6 +10,31 @@ template<typename T>
 Vector<T> banded_sle(const std::size_t l, const std::size_t u, Matrix<T>& banded_matrix, Vector<T>& b){//, const std::size_t param){
 
     Vector<T> solution(b);
+
+    if(u == 0 && l == 0){
+        /*
+
+        banded_matrix = [
+            [c, c, c, c, *],
+            [b, b, b, b, b],
+            [*, a, a, a, a],
+        ]
+
+        [b, c, 0,    ....]   [d]
+        [a, b, c,    ....]   [d]
+        [0, a, b, c, ....]   [d]
+        .................. = ...
+        [......., a, b, c]   [d]
+        [.........., a, b]   [d]        
+        
+        
+        */
+        if(true){
+            for(std::size_t j = 0; j < solution.size(); ++j){
+                solution[j] = b[j] / banded_matrix[0][j];
+            }
+        }        
+    }  
     
     if(u == 1 && l == 1){
         /*
@@ -34,12 +59,12 @@ Vector<T> banded_sle(const std::size_t l, const std::size_t u, Matrix<T>& banded
             std::vector<T> beta;
             T tmp = 0;
 
-            alpha.push_back( - banded_matrix[0][0] / banded_matrix[1][0] );
-            beta.push_back(b[0] / banded_matrix[1][0]);
+            alpha.push_back( - banded_matrix[1][0] / banded_matrix[0][0] );
+            beta.push_back(b[0] / banded_matrix[0][0]);
 
             for(std::size_t i = 1; i < b.size(); ++i){
-                tmp = banded_matrix[1][i] + banded_matrix[2][i] * alpha[i - 1];
-                (i < b.size() - 1) ? alpha.push_back( - banded_matrix[0][i] / tmp ) : alpha.push_back(0.);
+                tmp = banded_matrix[0][i] + banded_matrix[2][i] * alpha[i - 1];
+                (i < b.size() - 1) ? alpha.push_back( - banded_matrix[1][i] / tmp ) : alpha.push_back(0.);
                 beta.push_back( (b[i] - banded_matrix[2][i] * beta[i-1]) / tmp );
             }
             
@@ -54,6 +79,8 @@ Vector<T> banded_sle(const std::size_t l, const std::size_t u, Matrix<T>& banded
         }        
         
     }  
+
+    
     
     return solution;
 
