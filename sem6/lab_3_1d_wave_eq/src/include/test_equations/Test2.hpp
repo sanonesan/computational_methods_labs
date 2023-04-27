c23e41c8f7244d42fbdf74d58cc9a04bbcdf5c47
sanonesan <ezcudze@gmail.com>
2023-04-27 22:48:05 +0300

    save

--- sem6/lab_3_1d_wave_eq/src/include/test_equations/Test2.hpp
+++ sem6/lab_3_1d_wave_eq/src/include/test_equations/Test2.hpp
@@ -19,7 +19,48 @@ class Test2: virtual public Class_1d_wave_equation<T>{
             //test name
             this->_name = std::string (__func__);
 
-            this->DEFAULT_TEST();
+            //material parameters
+            this->_a = 1.;
+            
+            //Space
+            this->_x0 = 0.;
+            this->_xL = 2.;
+            this->_h = 0.01;
+
+            //Time
+            this->_start_time = 0.;
+            this->_end_time = 5.;
+            this->_tau = 0.005;
+
+            // Boundary {u(x0, t), u(xL, t)}
+            auto u_0_t = [this](const T x, const T t) -> T{
+                return 0.;
+            };
+
+            auto u_L_t = [this](const T x, const T t) -> T{
+                return 0.;
+            };
+            
+            this->_boundary_conditions.clear();
+            this->_boundary_conditions.shrink_to_fit();
+            this->_boundary_conditions.push_back(u_0_t);
+            this->_boundary_conditions.push_back(u_L_t);
+
+            // Initial u(x, 0)
+            auto u_x_0 = [this](const T x, const T t) -> T{
+                return x * (this->_xL - x);
+            };
+
+            // du/dt(x, 0)
+            auto du_dt_x_0 = [this](const T x, const T t) -> T{
+                return 0.;
+            };
+
+            this->_initial_conditions.clear();
+            this->_initial_conditions.shrink_to_fit();
+            this->_initial_conditions.push_back(u_x_0);
+            this->_initial_conditions.push_back(du_dt_x_0);
+
         }
 
 };
--- sem6/lab_3_1d_wave_eq/src/main.cpp
+++ sem6/lab_3_1d_wave_eq/src/main.cpp
@@ -2,10 +2,11 @@
 
 #include "./include/Solver_1d_wave_eq.hpp"
 #include "./include/test_equations/Test1.hpp"
+#include "./include/test_equations/Test2.hpp"
 
 int main(int args, char **argv){
 
-    typedef long double T;
+    typedef double T;
     
     Solver_1d_wave_eq<T> solver;
     solver.notifications = true;
@@ -15,8 +16,14 @@ int main(int args, char **argv){
     solver.file_name = test1._name;
 
     solver.solve_eq(test1);
-    // solver.solve_eq(test1, 1);
-    // solver.solve_eq(test1, 0.5);
+    std::cout << "\n";
+
+
+    Test2<T> test2;
+    solver.output_folder = "../output/Test2/";
+    solver.file_name = test2._name;
+
+    solver.solve_eq(test2);
     std::cout << "\n";
 
     
